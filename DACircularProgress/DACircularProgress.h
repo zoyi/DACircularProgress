f145065b27938e1df189830f1423d86e613bbd29
Wooyoung Chung <yakle1218@gmail.com>
2017-03-16 19:09:55 +0900

    Modified to make compatible with Carthage

---
+++ DACircularProgress/DACircularProgress.h
@@ -0,0 +1,19 @@
+//
+//  DACircularProgress.h
+//  DACircularProgress
+//
+//  Created by Haeun Chung on 16/03/2017.
+//  Copyright © 2017 ZOYI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DACircularProgress.
+FOUNDATION_EXPORT double DACircularProgressVersionNumber;
+
+//! Project version string for DACircularProgress.
+FOUNDATION_EXPORT const unsigned char DACircularProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DACircularProgress/PublicHeader.h>
+
+
